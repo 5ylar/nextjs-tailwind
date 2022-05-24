b3a7cebe22dcf90fb0dcbf3511dca107b30f5bcb
5ylar <forzepmono@gmail.com>
2022-05-24 18:35:22 +0700

    tailwind setup

---
+++ pages/index.tsx
@@ -0,0 +1,11 @@
+import type { NextPage } from 'next'
+
+const Home: NextPage = () => {
+  return (
+    <div >
+      <h1 className='text-todo text-3xl font-bold underline'>Test</h1>
+    </div>
+  )
+}
+
+export default Home
